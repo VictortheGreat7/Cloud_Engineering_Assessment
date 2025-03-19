0d650c82937d2c23b90923a9f4457e8bb553719d
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2025-03-19 12:54:21 +0100

    Cert-Manager Check 14

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -25,26 +25,55 @@ provider "azurerm" {
   subscription_id = "d31507f4-324c-4bd1-abe1-5cdf45cba77d"
 }
 
+# First, convert the raw YAML string to a parsed object using yamldecode
+locals {
+  kube_config = yamldecode(azurerm_kubernetes_cluster.capstone.kube_admin_config_raw)
+}
+
+# Now you can reference the config elements from the parsed structure
 provider "kubernetes" {
-  host                   = azurerm_kubernetes_cluster.capstone.kube_admin_config_raw.host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.capstone.kube_admin_config_raw.client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.capstone.kube_admin_config_raw.client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.capstone.kube_admin_config_raw.cluster_ca_certificate)
+  host                   = local.kube_config.clusters[0].cluster.server
+  client_certificate     = base64decode(local.kube_config.users[0].user["client-certificate-data"])
+  client_key             = base64decode(local.kube_config.users[0].user["client-key-data"])
+  cluster_ca_certificate = base64decode(local.kube_config.clusters[0].cluster["certificate-authority-data"])
 }
 
 provider "helm" {
   kubernetes {
-    host                   = azurerm_kubernetes_cluster.capstone.kube_admin_config_raw.host
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.capstone.kube_admin_config_raw.client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.capstone.kube_admin_config_raw.client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.capstone.kube_admin_config_raw.cluster_ca_certificate)
+    host                   = local.kube_config.clusters[0].cluster.server
+    client_certificate     = base64decode(local.kube_config.users[0].user["client-certificate-data"])
+    client_key             = base64decode(local.kube_config.users[0].user["client-key-data"])
+    cluster_ca_certificate = base64decode(local.kube_config.clusters[0].cluster["certificate-authority-data"])
   }
 }
 
 provider "kubectl" {
-  host                   = azurerm_kubernetes_cluster.capstone.kube_admin_config_raw.host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.capstone.kube_admin_config_raw.client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.capstone.kube_admin_config_raw.client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.capstone.kube_admin_config_raw.cluster_ca_certificate)
-  # load_admin_config_file       = false
+  host                   = local.kube_config.clusters[0].cluster.server
+  client_certificate     = base64decode(local.kube_config.users[0].user["client-certificate-data"])
+  client_key             = base64decode(local.kube_config.users[0].user["client-key-data"])
+  cluster_ca_certificate = base64decode(local.kube_config.clusters[0].cluster["certificate-authority-data"])
 }
+
+# provider "kubernetes" {
+#   host                   = azurerm_kubernetes_cluster.capstone.kube_admin_config_raw.host
+#   client_certificate     = base64decode(azurerm_kubernetes_cluster.capstone.kube_admin_config_raw.client_certificate)
+#   client_key             = base64decode(azurerm_kubernetes_cluster.capstone.kube_admin_config_raw.client_key)
+#   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.capstone.kube_admin_config_raw.cluster_ca_certificate)
+# }
+
+# provider "helm" {
+#   kubernetes {
+#     host                   = azurerm_kubernetes_cluster.capstone.kube_admin_config_raw.host
+#     client_certificate     = base64decode(azurerm_kubernetes_cluster.capstone.kube_admin_config_raw.client_certificate)
+#     client_key             = base64decode(azurerm_kubernetes_cluster.capstone.kube_admin_config_raw.client_key)
+#     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.capstone.kube_admin_config_raw.cluster_ca_certificate)
+#   }
+# }
+
+# provider "kubectl" {
+#   host                   = azurerm_kubernetes_cluster.capstone.kube_admin_config_raw.host
+#   client_certificate     = base64decode(azurerm_kubernetes_cluster.capstone.kube_admin_config_raw.client_certificate)
+#   client_key             = base64decode(azurerm_kubernetes_cluster.capstone.kube_admin_config_raw.client_key)
+#   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.capstone.kube_admin_config_raw.cluster_ca_certificate)
+#   # load_admin_config_file       = false
+# }
