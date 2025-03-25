6884e41a2e27566538a72d9084b103fd03c82441
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2025-03-25 09:50:32 +0100

    Domain Configuration Check 4: Let's Encrypt Check 3

--- terraform/deploy.tf
+++ terraform/deploy.tf
@@ -177,7 +177,7 @@ resource "kubernetes_ingress_v1" "time_api" {
     }
   }
 
-  depends_on = [kubernetes_service.time_api, module.certmanager, resource.kubectl_manifest.cluster_issuer]
+  depends_on = [kubernetes_service.time_api, module.certmanager]
 }
 
 # Load Test Job
