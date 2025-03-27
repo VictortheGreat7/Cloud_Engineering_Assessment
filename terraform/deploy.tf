c178d7abac44060001141930ba6ffd3ef4fcba3e
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2025-03-27 21:04:54 +0100

    Naming Convention and RG Permission Test 5

--- terraform/deploy.tf
+++ terraform/deploy.tf
@@ -10,8 +10,8 @@ module "nginx-controller" {
 
 resource "helm_release" "cert_manager" {
   name             = "cert-manager"
-  repository       = "https://charts.jetstack.io"
   chart            = "cert-manager"
+  repository       = "https://charts.jetstack.io"
   namespace        = "cert-manager"
   create_namespace = true
   version          = "v1.16.0"
