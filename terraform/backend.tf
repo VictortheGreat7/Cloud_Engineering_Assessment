386a8b6f1133c7fb96f3770e539881b8b81780bb
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2025-03-27 20:28:36 +0100

    Naming Convention and RG Permission Test 4

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 # This file is used to configure the backend for the terraform state file.
 terraform {
   backend "azurerm" {
-    resource_group_name  = "${random_pet.time_api.id}-backend-rg"
+    resource_group_name  = "backend-rg"
     storage_account_name = "bunnybackend349"
     container_name       = "tfstate"
     key                  = "test.terraform.tfstate"
