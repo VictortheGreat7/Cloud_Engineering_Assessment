c63fa909d55c125356bb7cb9f4ac591f9469b3e3
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2025-03-31 15:34:08 +0100

    Name Server List Population Test 1

--- terraform/domain.sh
+++ terraform/domain.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 set -eu
 
+USERNAME="VictortheGreat"
+TOKEN="a44c00dcf5b1efc0ecdc3ab2884a146cdf0c3c82"
+DOMAIN="mywonder.works"
+
+NAMESERVERS=$(terraform output -json name_servers)
+
 # API endpoint for Name.com nameserver updates
-API_URL="https://api.name.com/v4/domains/$DOMAIN:setNameservers"
+API_URL="https://api.name.com/v4/domains/$DOMAIN/nameservers"
+
+AZURE_NS1=$(echo "$NAMESERVERS" | grep -v 'terraform-bin' | grep -v 'Terraform exited with code' | grep -oP '"\K[^"]+(?=")' | head -n 1 | sed 's/,//; s/\.$//' | grep -v '^0$')
+AZURE_NS2=$(echo "$NAMESERVERS" | grep -v 'terraform-bin' | grep -v 'Terraform exited with code' | grep -oP '"\K[^"]+(?=")' | head -n 2 | tail -n 1 | sed 's/,//; s/\.$//' | grep -v '^0$')
+AZURE_NS3=$(echo "$NAMESERVERS" | grep -v 'terraform-bin' | grep -v 'Terraform exited with code' | grep -oP '"\K[^"]+(?=")' | head -n 3 | tail -n 1 | sed 's/,//; s/\.$//' | grep -v '^0$')
+AZURE_NS4=$(echo "$NAMESERVERS" | grep -v 'terraform-bin' | grep -v 'Terraform exited with code' | grep -oP '"\K[^"]+(?=")' | head -n 4 | tail -n 1 | sed 's/,//; s/\.$//' | grep -v '^0$')
 
 # Construct the JSON payload for the nameserver update
 PAYLOAD=$(cat <<-EOF
@@ -18,7 +29,7 @@ EOF
 )
 
 # Update nameservers using the Name.com API
-curl -X POST "$API_URL" \
+curl -X PUT "$API_URL" \
   -u "$USERNAME:$TOKEN" \
   -H "Content-Type: application/json" \
   -d "$PAYLOAD"
