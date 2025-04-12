c0d2861abd41607d473ae8709ec95185a29b3908
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2025-04-12 14:30:06 +0100

    Workflow File Cleanup 3

--- terraform/bash_scripts/gh_secret.sh
+++ terraform/bash_scripts/gh_secret.sh
@@ -4,18 +4,30 @@ set -eu
 # Define repository name for reuse
 REPO_NAME=""
 
+# Azure/ARM credentials
+CLIENT_ID=""
+CLIENT_SECRET=""
+SUBSCRIPTION_ID=""
+TENANT_ID=""
+
+# Azure credentials as JSON
+AZURE_CREDENTIALS=$(cat <<EOF
+{
+  "clientId": "${CLIENT_ID}",
+  "clientSecret": "${CLIENT_SECRET}",
+  "subscriptionId": "${SUBSCRIPTION_ID}",
+  "tenantId": "${TENANT_ID}"
+}
+EOF
+)
+
 # Declare an associative array to hold secrets and their corresponding values
 declare -A secrets=(
-  ["AZURE_CREDENTIALS"]='{
-    "clientId": "",
-    "clientSecret": "",
-    "subscriptionId": "",
-    "tenantId": ""
-  }'
-  ["ARM_CLIENT_ID"]=""
-  ["ARM_CLIENT_SECRET"]=""
-  ["ARM_SUBSCRIPTION_ID"]=""
-  ["ARM_TENANT_ID"]=""
+  ["AZURE_CREDENTIALS"]="${AZURE_CREDENTIALS}"
+  ["ARM_CLIENT_ID"]="${CLIENT_ID}"
+  ["ARM_CLIENT_SECRET"]="${CLIENT_SECRET}"
+  ["ARM_SUBSCRIPTION_ID"]="${SUBSCRIPTION_ID}"
+  ["ARM_TENANT_ID"]="${TENANT_ID}"
   ["MY_USER_OBJECT_ID"]=""
   ["DOMAIN"]=""
   ["DOMAIN_API_USERNAME"]=""
