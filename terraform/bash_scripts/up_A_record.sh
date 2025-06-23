a862cc5a1db122b90d63155b774c7e4b3cf1abd0
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2025-06-23 11:37:21 +0100

    Checking issues with aks_get_creds

--- terraform/bash_scripts/up_A_record.sh
+++ terraform/bash_scripts/up_A_record.sh
@@ -4,7 +4,7 @@ set -eu
 # DNS record details
 RECORD_TYPE="A"
 HOST="api"
-ANSWER=$(kubectl get svc -n kube-system ingress-nginx-controller -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+ANSWER=$(kubectl get svc -n nginx-ingress ingress-nginx-controller -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 TTL="300"
 
 # API endpoint for A record addition
