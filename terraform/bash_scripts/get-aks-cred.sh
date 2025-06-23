89921dcfccc7acaeeff01650079ef0012017a4cd
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2025-06-23 10:55:45 +0100

    Checking issues with aks_get_creds

--- terraform/bash_scripts/get-aks-cred.sh
+++ terraform/bash_scripts/get-aks-cred.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-RESOURCE_GROUP=$(terraform output -raw aks_resource_group | grep -v 'terraform-bin' | grep -v 'Terraform exited with code' | awk -F': ' '{print $2}' | grep -v '^0$')
-CLUSTER_NAME=$(terraform output -raw aks_cluster_name | grep -v 'terraform-bin' | grep -v 'Terraform exited with code' | awk -F': ' '{print $2}' | grep -v '^0$')
+RESOURCE_GROUP=$(terraform output -raw aks_resource_group)
+CLUSTER_NAME=$(terraform output -raw aks_cluster_name)
 
 az aks get-credentials --resource-group $RESOURCE_GROUP --name $CLUSTER_NAME
