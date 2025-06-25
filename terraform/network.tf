215f1b66941af49a36ff8a7be82e8beb90c2caa0
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2025-06-25 13:13:52 +0100

    Loosened nsg rules

--- terraform/network.tf
+++ terraform/network.tf
@@ -35,6 +35,18 @@ resource "azurerm_network_security_group" "time_api_nsg" {
   }
 
   security_rule {
+    name                       = "allow-http-access"
+    priority                   = 103
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
     name                       = "allow-vnet-inbound"
     priority                   = 102
     direction                  = "Inbound"
