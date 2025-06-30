1c1cc090369ab27eccd38d88a50086c499e76406
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2025-06-30 12:17:38 +0100

    Investigating NGINX endpoints issue 3

--- terraform/microservices/deploy.tf
+++ terraform/microservices/deploy.tf
@@ -110,6 +110,12 @@ resource "kubernetes_job_v1" "time_api_loadtest" {
   depends_on = [kubernetes_service_v1.time_api]
 }
 
+resource "time_sleep" "wait_for_nginx" {
+  create_duration = "120s"  # Wait 2 minutes
+
+  depends_on = [module.nginx-controller]
+}
+
 # This makes the API service accessible from outside the cluster.
 resource "kubernetes_ingress_v1" "time_api" {
   metadata {
@@ -139,5 +145,5 @@ resource "kubernetes_ingress_v1" "time_api" {
     }
   }
 
-  depends_on = [kubernetes_service_v1.time_api, azurerm_dashboard_grafana.timeapi_grafana]
+  depends_on = [kubernetes_service_v1.time_api, azurerm_dashboard_grafana.timeapi_grafana, time_sleep.wait_for_nginx]
 }
