2b2c39ff859a0228deb5cc6f66459f1ef50a0832
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2025-07-27 20:30:10 +0100

    Self-Hosted Runner Approach Attempt 6

--- terraform/deploy/provision.tf
+++ terraform/deploy/provision.tf
@@ -72,11 +72,9 @@ resource "azurerm_kubernetes_cluster" "time_api_cluster" {
 }
 
 provider "kubernetes" {
-  host                   = azurerm_kubernetes_cluster.time_api_cluster.kube_config[0].host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.time_api_cluster.kube_config[0].client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.time_api_cluster.kube_config[0].client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.time_api_cluster.kube_config[0].cluster_ca_certificate)
+  config_path = pathexpand("~/.kube/config")
 }
+
 
 provider "helm" {
   kubernetes {
