c33d8b685016cad27fcba48303520ffc85cd8c8a
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2025-07-27 08:24:13 +0100

    Self-Hosted Runner Approach Attempt 2

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,10 +8,6 @@ output "natgtw_ip" {
   value = azurerm_public_ip.time_api_public_ip
 }
 
-output "runner_vm_ip" {
-  value = azurerm_public_ip.gha_public_ip.ip_address
-}
-
 output "ssh_command" {
   value = "ssh -i ssh_keys/id_rsa azureuser@${data.azurerm_public_ip.gha_dynamic_ip.ip_address}"
 }
